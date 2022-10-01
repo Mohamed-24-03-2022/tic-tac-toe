bc7560d866aaea64f2b04ed143cfc14eed24052a
mohamed200111 <mohakatalonya10@gmail.com>
2022-10-01 22:35:00 +0100

    Add custom player names

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tic-tac-toe
 
-<a href ="https://Mohamed-24-03-2022.github.io/tic-tac-toe/"> <strong> Live Demo</strong> </a>   
+Applying my newly acquired knowledge about factory function,  
+module pattern and immediately invoked function expression (IIFE).
+
+<a href ="https://Mohamed-24-03-2022.github.io/tic-tac-toe/"> <strong> Live Demo</strong> </a>
--- index.js
+++ index.js
@@ -6,24 +6,50 @@ function Player(name, choice, order) {
     };
     return player;
 }
+const choosePlayerNames = () => {
+    document.body.style.backgroundColor = "#000000";
+    const startButton = document.querySelector(".form-container button");
+    startButton.addEventListener("click", () => {
+        const formContainer = document.querySelector(".form-container");
+        const playerOneNameInput = document.querySelector("#player-one-name");
+        const playerTwoNameInput = document.querySelector("#player-two-name");
+        const playerNameParagraph = document.querySelectorAll(".player-box p");
+        if (!playerOneNameInput.value || !playerTwoNameInput.value) {
+            return;
+        } else {
+            gameboard.playerOne.name = playerOneNameInput.value;
+            gameboard.playerTwo.name = playerTwoNameInput.value;
+            playerNameParagraph[0].textContent = gameboard.playerOne.name;
+            playerNameParagraph[1].textContent = gameboard.playerTwo.name;
+            formContainer.style.visibility = "hidden";
+            startButton.style.visibility = "hidden";
+            formContainer.remove();
+            startButton.remove();
+            document.body.style.backgroundColor = "";
+            displayController.boxesContainer.addEventListener("click", displayController.populationBoxes);
+        }
+    });
+};
 const gameboard = (() => {
     const playerOne = Player("Player one", "x", 1);
     const playerTwo = Player("Player two", "o", 2);
-    const score = {
+    choosePlayerNames();
+
+    const content = {
         playerOne: [],
         playerTwo: [],
     };
     const generatePlayerChoice = (box) => {
         if (box.textContent === "x" || box.textContent === "o") return;
-        const xNumber = score.playerOne.length;
-        const oNumber = score.playerTwo.length;
+        const xNumber = content.playerOne.length;
+        const oNumber = content.playerTwo.length;
         if (xNumber === 0 || xNumber === oNumber) {
-            score.playerOne.push(playerOne.choice);
+            content.playerOne.push(playerOne.choice);
         } else if (xNumber > oNumber) {
-            score.playerTwo.push(playerTwo.choice);
+            content.playerTwo.push(playerTwo.choice);
         }
     };
-    return { score, generatePlayerChoice, playerOne, playerTwo };
+    return { content, generatePlayerChoice, playerOne, playerTwo };
 })();
 const changeContainerVisibility = (() => {
     const resultContainer = document.querySelector(".result-container");
@@ -37,27 +63,27 @@ const changeContainerVisibility = (() => {
         resetButton.style.visibility = "visible";
     };
     hideContainer();
-    return { hideContainer, showContainer };
+    return { hideContainer, showContainer, resultContainer };
 })();
 const displayController = (() => {
     const boxesContainer = document.querySelector(".boxes-container");
-    //* using event delegation
     const populationBoxes = (e) => {
         gameboard.generatePlayerChoice(e.target);
         const box = e.target;
-        const score = gameboard.score;
+        const content = gameboard.content;
         const choiceText = document.createElement("span");
+        const xNumber = content.playerOne.length;
+        const oNumber = content.playerTwo.length;
         if (box.textContent === "x" || box.textContent === "o") {
             return;
-        } else if (score.playerOne.length > score.playerTwo.length) {
-            choiceText.textContent = score.playerOne[score.playerOne.length - 1];
-        } else if (score.playerOne.length === score.playerTwo.length) {
-            choiceText.textContent = score.playerTwo[score.playerTwo.length - 1];
+        } else if (xNumber > oNumber) {
+            choiceText.textContent = content.playerOne[content.playerOne.length - 1];
+        } else if (xNumber === oNumber) {
+            choiceText.textContent = content.playerTwo[content.playerTwo.length - 1];
         }
         box.appendChild(choiceText);
         chooseWinner();
     };
-    boxesContainer.addEventListener("click", populationBoxes);
     return { boxesContainer, populationBoxes };
 })();
 const chooseWinner = () => {
@@ -76,7 +102,7 @@ const chooseWinner = () => {
         (boxContent(2) === "x" && boxContent(4) === "x" && boxContent(6) === "x")
     ) {
         console.log("YOU WIN");
-        resettingGame(`${gameboard.playerOne.name} has won.`);
+        resetGame(`${gameboard.playerOne.name} has won.`);
     } else if (
         (boxContent(0) === "o" && boxContent(1) === "o" && boxContent(2) === "o") ||
         (boxContent(3) === "o" && boxContent(4) === "o" && boxContent(5) === "o") ||
@@ -88,7 +114,7 @@ const chooseWinner = () => {
         (boxContent(2) === "o" && boxContent(4) === "o" && boxContent(6) === "o")
     ) {
         console.log("you lost");
-        resettingGame("You Lost");
+        resetGame("You Lost");
     } else if (
         boxContent(0) &&
         boxContent(1) &&
@@ -101,18 +127,19 @@ const chooseWinner = () => {
         boxContent(8)
     ) {
         console.log("It's a tie");
-        resettingGame("It's a tie");
+        resetGame("It's a tie");
     }
 }
-const resettingGame = (result) => {
+const resetGame = (result) => {
     const resultParagraph = document.querySelector(".result-container p");
     const resetButton = document.querySelector(".result-container button");
+
     resultParagraph.textContent = result;
     changeContainerVisibility.showContainer();
     displayController.boxesContainer.removeEventListener("click", displayController.populationBoxes);
     resetButton.addEventListener("click", () => {
-        gameboard.score.playerOne = [];
-        gameboard.score.playerTwo = [];
+        gameboard.content.playerOne = [];
+        gameboard.content.playerTwo = [];
         document.querySelectorAll(".box span").forEach((choiceText) => {
             choiceText.textContent = "";
             choiceText.remove();
@@ -121,3 +148,24 @@ const resettingGame = (result) => {
         displayController.boxesContainer.addEventListener("click", displayController.populationBoxes);
     });
 };
+const restartGame = (() => {
+    const restartButton = document.querySelector(".restart-container button");
+
+    restartButton.addEventListener("click", () => {
+        if (changeContainerVisibility.resultContainer.style.visibility === "visible") {
+            return;
+        }
+        gameboard.content.playerOne = [];
+        gameboard.content.playerTwo = [];
+        document.querySelectorAll(".box span").forEach((choiceText) => {
+            choiceText.textContent = "";
+            document.querySelectorAll(".box span").forEach((choiceText) => {
+                choiceText.textContent = "";
+                choiceText.remove();
+            });
+        });
+    });
+})();
+
+//todo highlight player turn
+//todo add computer player
