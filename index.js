8f42c003a1ae0c50f6ac6c58d7e51eae95ef3916
mohamed200111 <mohakatalonya10@gmail.com>
2022-10-03 20:56:38 +0100

    Add computer player

--- index.js
+++ index.js
@@ -9,24 +9,42 @@ function Player(name, choice, order) {
 const choosePlayerNames = () => {
     document.body.style.backgroundColor = "#000000";
     const startButton = document.querySelector(".form-container button");
+    const playerTwoNameInput = document.querySelector("#player-two-name");
+    const isComputer = document.querySelector("#is-computer");
+    isComputer.addEventListener("input", () => {
+        if (isComputer.checked === true) {
+            playerTwoNameInput.disabled = true;
+            playerTwoNameInput.value = "Computer";
+        } else {
+            playerTwoNameInput.disabled = false;
+            playerTwoNameInput.value = "";
+        }
+    });
+
     startButton.addEventListener("click", () => {
         const formContainer = document.querySelector(".form-container");
         const playerOneNameInput = document.querySelector("#player-one-name");
-        const playerTwoNameInput = document.querySelector("#player-two-name");
-        const playerNameParagraph = document.querySelectorAll(".player-box p");
-        if (!playerOneNameInput.value || !playerTwoNameInput.value) {
+        const playersNameParagraph = document.querySelectorAll(".player-box p");
+        if (
+            !playerOneNameInput.value ||
+            (!playerTwoNameInput.value && isComputer.checked === false)
+        ) {
             return;
         } else {
+            document.querySelector(".logo-box .player-one").style.color = "#ffc442";
             gameboard.playerOne.name = playerOneNameInput.value;
             gameboard.playerTwo.name = playerTwoNameInput.value;
-            playerNameParagraph[0].textContent = gameboard.playerOne.name;
-            playerNameParagraph[1].textContent = gameboard.playerTwo.name;
+            playersNameParagraph[0].textContent = gameboard.playerOne.name;
+            playersNameParagraph[1].textContent = gameboard.playerTwo.name;
             formContainer.style.visibility = "hidden";
             startButton.style.visibility = "hidden";
             formContainer.remove();
             startButton.remove();
             document.body.style.backgroundColor = "";
-            displayController.boxesContainer.addEventListener("click", displayController.populationBoxes);
+            displayController.boxesContainer.addEventListener(
+                "click",
+                displayController.populationBoxes
+            );
         }
     });
 };
@@ -44,8 +62,12 @@ const gameboard = (() => {
         const xNumber = content.playerOne.length;
         const oNumber = content.playerTwo.length;
         if (xNumber === 0 || xNumber === oNumber) {
+            document.querySelector(".logo-box .player-one").style.color = "#333";
+            document.querySelector(".logo-box .player-two").style.color = "#ffc442";
             content.playerOne.push(playerOne.choice);
         } else if (xNumber > oNumber) {
+            document.querySelector(".logo-box .player-one").style.color = "#ffc442";
+            document.querySelector(".logo-box .player-two").style.color = "#333";
             content.playerTwo.push(playerTwo.choice);
         }
     };
@@ -67,22 +89,51 @@ const changeContainerVisibility = (() => {
 })();
 const displayController = (() => {
     const boxesContainer = document.querySelector(".boxes-container");
+    const boxes = document.querySelectorAll(".box");
     const populationBoxes = (e) => {
-        gameboard.generatePlayerChoice(e.target);
-        const box = e.target;
-        const content = gameboard.content;
-        const choiceText = document.createElement("span");
-        const xNumber = content.playerOne.length;
-        const oNumber = content.playerTwo.length;
-        if (box.textContent === "x" || box.textContent === "o") {
+        boxes.forEach((box) => {
+            if (e.target === box) {
+                gameboard.generatePlayerChoice(e.target);
+                const content = gameboard.content;
+                const choiceText = document.createElement("span");
+                const xNumber = content.playerOne.length;
+                const oNumber = content.playerTwo.length;
+                if (box.textContent === "x" || box.textContent === "o") {
+                    return;
+                } else if (xNumber > oNumber) {
+                    choiceText.textContent = content.playerOne[content.playerOne.length - 1];
+                    if (gameboard.playerTwo.name === "Computer") {
+                        setTimeout(() => generatePcChoice(), 150);
+                    }
+                } else if (xNumber === oNumber) {
+                    choiceText.textContent = content.playerTwo[content.playerTwo.length - 1];
+                }
+                box.appendChild(choiceText);
+                chooseWinner();
+            }
+        });
+    };
+    const generatePcChoice = () => {
+        const randChoice = Math.floor(Math.random() * 9);
+        if (
+            boxes[0].textContent !== "" &&
+            boxes[1].textContent !== "" &&
+            boxes[2].textContent !== "" &&
+            boxes[3].textContent !== "" &&
+            boxes[4].textContent !== "" &&
+            boxes[5].textContent !== "" &&
+            boxes[6].textContent !== "" &&
+            boxes[7].textContent !== "" &&
+            boxes[8].textContent !== ""
+        ) {
             return;
-        } else if (xNumber > oNumber) {
-            choiceText.textContent = content.playerOne[content.playerOne.length - 1];
-        } else if (xNumber === oNumber) {
-            choiceText.textContent = content.playerTwo[content.playerTwo.length - 1];
         }
-        box.appendChild(choiceText);
-        chooseWinner();
+        if (boxes[randChoice].textContent === "") {
+            boxes[randChoice].click();
+        } else if (boxes[randChoice].textContent !== "") {
+            generatePcChoice();
+            console.log("WRONG BOX OOPS");
+        }
     };
     return { boxesContainer, populationBoxes };
 })();
@@ -102,6 +153,8 @@ const chooseWinner = () => {
         (boxContent(2) === "x" && boxContent(4) === "x" && boxContent(6) === "x")
     ) {
         console.log("YOU WIN");
+        document.querySelector(".logo-box .player-one").style.color = "#ffc442";
+        document.querySelector(".logo-box .player-two").style.color = "#333";
         resetGame(`${gameboard.playerOne.name} has won.`);
     } else if (
         (boxContent(0) === "o" && boxContent(1) === "o" && boxContent(2) === "o") ||
@@ -114,6 +167,8 @@ const chooseWinner = () => {
         (boxContent(2) === "o" && boxContent(4) === "o" && boxContent(6) === "o")
     ) {
         console.log("you lost");
+        document.querySelector(".logo-box .player-one").style.color = "#333";
+        document.querySelector(".logo-box .player-two").style.color = "#ffc442";
         resetGame("You Lost");
     } else if (
         boxContent(0) &&
@@ -127,45 +182,56 @@ const chooseWinner = () => {
         boxContent(8)
     ) {
         console.log("It's a tie");
+        document.querySelector(".logo-box .player-one").style.color = "#ffc442";
+        document.querySelector(".logo-box .player-two").style.color = "#ffc442";
         resetGame("It's a tie");
     }
-}
+};
 const resetGame = (result) => {
     const resultParagraph = document.querySelector(".result-container p");
     const resetButton = document.querySelector(".result-container button");
-
     resultParagraph.textContent = result;
     changeContainerVisibility.showContainer();
-    displayController.boxesContainer.removeEventListener("click", displayController.populationBoxes);
+    displayController.boxesContainer.removeEventListener(
+        "click",
+        displayController.populationBoxes
+    );
     resetButton.addEventListener("click", () => {
+        gameboard.content.playerOne = [];
+        gameboard.content.playerTwo = [];
+        document.querySelector(".logo-box .player-one").style.color = "#ffc442";
+        document.querySelector(".logo-box .player-two").style.color = "#333";
+        document.querySelectorAll(".box span").forEach((choiceText) => {
+            choiceText.textContent = "";
+            choiceText.remove();
+        });
+        changeContainerVisibility.hideContainer();
+        displayController.boxesContainer.addEventListener(
+            "click",
+            displayController.populationBoxes
+        );
+    });
+};
+const restartGame = (() => {
+    const restartButton = document.querySelector(".restart-container button");
+    restartButton.addEventListener("click", () => {
+        if (
+            changeContainerVisibility.resultContainer.style.visibility === "visible"
+        ) {
+            return;
+        }
+        document.querySelector(".logo-box .player-one").style.color = "#ffc442";
+        document.querySelector(".logo-box .player-two").style.color = "#333";
         gameboard.content.playerOne = [];
         gameboard.content.playerTwo = [];
         document.querySelectorAll(".box span").forEach((choiceText) => {
             choiceText.textContent = "";
             choiceText.remove();
         });
-        changeContainerVisibility.hideContainer();
-        displayController.boxesContainer.addEventListener("click", displayController.populationBoxes);
-    });
-};
-const restartGame = (() => {
-    const restartButton = document.querySelector(".restart-container button");
-
-    restartButton.addEventListener("click", () => {
-        if (changeContainerVisibility.resultContainer.style.visibility === "visible") {
-            return;
-        }
-        gameboard.content.playerOne = [];
-        gameboard.content.playerTwo = [];
-        document.querySelectorAll(".box span").forEach((choiceText) => {
-            choiceText.textContent = "";
-            document.querySelectorAll(".box span").forEach((choiceText) => {
-                choiceText.textContent = "";
-                choiceText.remove();
-            });
-        });
     });
 })();
 
+//todo read my documentation in oneNote
 //todo highlight player turn
 //todo add computer player
+//todo try remove IIFE if it's not necessary (by trying to delete and see the feedback)
